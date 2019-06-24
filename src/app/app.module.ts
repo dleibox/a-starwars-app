68904b295f43de0c48c0bab28c82d2b29d1814e8
Daniel Lei <dleibox@gmail.com>
2019-06-24 13:17:31 -0400

    Add store-devtools and updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceModule } from './service/service.module';
+
+import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
 import { AppBottomsheetComponent } from './app-bottomsheet.component';
@@ -23,6 +26,10 @@ import { AppBottomsheetComponent } from './app-bottomsheet.component';
     AppMaterialModule,
     AppRoutingModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
     EffectsModule.forRoot([]),
     ServiceModule.forRoot(),
   ],
