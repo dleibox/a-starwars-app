ae33fb485b961f3b22edcae7b32de32ad06e084d
Daniel Lei <dleibox@gmail.com>
2019-06-24 14:00:33 -0400

    Updates

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AppMaterialModule } from '../app-material.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
+    AppMaterialModule,
     RouterModule.forChild([{ path: '', component: HomeComponent }]),
   ]
 })
